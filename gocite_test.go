73d91a2fff65cca9083e4f00646b90a4fa5edaf2
ThomasK81 <thomas.koentges@uni-leipzig.de>
2018-05-29 13:05:28 +0200

    fix typo

--- gocite_test.go
+++ gocite_test.go
@@ -14,7 +14,7 @@ type testpair struct {
 
 var tests = []testpair{
 	{input: "urn:cts:collection:workgroup.work:1-27", outputSplit: gocite.CTSURN{ID: "urn:cts:collection:workgroup.work:1-27", Base: "urn", Protocol: "cts", Namespace: "collection", Work: "workgroup.work", Passage: "1-27"}, outputRange: true, outputCTS: true},
-	{input: "urn:cts:collection:workgroup.work:27.3", outputSplit: gocite.CTSURN{ID: "urn:cts:collection:workgroup.work:27.3", Base: "urn", Protocol: "cts", Namespace: "collectiom", Work: "workgroup.work", Passage: "27.3"}, outputRange: false, outputCTS: true},
+	{input: "urn:cts:collection:workgroup.work:27.3", outputSplit: gocite.CTSURN{ID: "urn:cts:collection:workgroup.work:27.3", Base: "urn", Protocol: "cts", Namespace: "collection", Work: "workgroup.work", Passage: "27.3"}, outputRange: false, outputCTS: true},
 	{input: "not:cts:collection:workgroup.work:27.3", outputSplit: gocite.CTSURN{ID: "not:cts:collection:workgroup.work:27.3", InValid: true}, outputRange: false, outputCTS: false}}
 
 func TestSplitCTS(t *testing.T) {
