b163f2254ec11f21234cb8c705ce49ca1800eb9c
ThomasK81 <thomas.koentges@uni-leipzig.de>
2018-05-29 12:51:25 +0200

    fix test

--- gocite_test.go
+++ gocite_test.go
@@ -7,24 +7,23 @@ import (
 )
 
 type testpair struct {
-	input  string
-	outputSplit gocite.CTSURN
+	input                  string
+	outputSplit            gocite.CTSURN
 	outputRange, outputCTS bool
 }
 
 var tests = []testpair{
 	{input: "urn:cts:collection:workgroup.work:1-27", outputSplit: gocite.CTSURN{ID: "urn:cts:collection:workgroup.work:1-27", Base: "urn", Protocol: "cts", Namespace: "workgroup", Work: "work", Passage: "1-27"}, outputRange: true, outputCTS: true},
 	{input: "urn:cts:collection:workgroup.work:27.3", outputSplit: gocite.CTSURN{ID: "urn:cts:collection:workgroup.work:27.3", Base: "urn", Protocol: "cts", Namespace: "workgroup", Work: "work", Passage: "27.3"}, outputRange: false, outputCTS: true},
-	{input: "not:cts:collection:workgroup.work:27.3", outputSplit: gocite.CTSURN{ID: "not:cts:collection:workgroup.work:27.3", InValid: true}, outputRange: false, outputCTS: false}
-}
+	{input: "not:cts:collection:workgroup.work:27.3", outputSplit: gocite.CTSURN{ID: "not:cts:collection:workgroup.work:27.3", InValid: true}, outputRange: false, outputCTS: false}}
 
 func TestSplitCTS(t *testing.T) {
 	for _, pair := range tests {
 		v := gocite.SplitCTS(pair.input)
-		if v != pair.output {
+		if v != pair.outputSplit {
 			t.Error(
 				"For", pair.input,
-				"expected", pair.output,
+				"expected", pair.outputSplit,
 				"got", v,
 			)
 		}
