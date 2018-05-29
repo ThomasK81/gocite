cb5647f73a1bdc7f6940d70c58b25f195ee7a4c2
koentges <koentges@Locals-MBP.fritz.box>
2018-05-29 19:56:02 +0200

    add DelPassage

--- gocite.go
+++ gocite.go
@@ -195,14 +195,17 @@ func GetPrev(id string, w Work) Passage {
 }
 
 // DelPassage deletes a Passage from a work by changing the references
-// to do...
-// func DelPassage(id string, w Work) Work {
-// 	result := w
-// 	index := GetIndexByID(id, w)
-// 	passage := GetPassageByInd(*index, w)
-// 	prevInd := passage.Prev.Index
-// 	nextInd := passage.Next.Index
-// 	prevPassage := GetPassageByInd(*prevInd, w)
-// 	nextPassage := GetPassageByInd(*nextInd, w)
-// 	prevPassage.
-// }
+func DelPassage(id string, w *Work) {
+	index := GetIndexByID(id, *w)
+	passage := GetPassageByInd(*index, *w)
+	prevInd := passage.Prev.Index
+	nextInd := passage.Next.Index
+	prevPassage := GetPassageByInd(*prevInd, *w)
+	nextPassage := GetPassageByInd(*nextInd, *w)
+	prevPassage.Next = passage.Next
+	nextPassage.Prev = passage.Prev
+	w.Passages[*index] = Passage{}
+	w.Passages[*prevInd] = prevPassage
+	w.Passages[*nextInd] = nextPassage
+	w.Ordered = false
+}
